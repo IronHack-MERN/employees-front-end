8ce3f5e28ecdec553753d48da89f650b2fe6db63
Elena Piaggio <elenapiaggio@gmail.com>
2020-07-19 20:57:03 +0200

    ADD getAllEmployees

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,9 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-class employeeService{
-    constructor(){
-        this.axios = axios.create({
-            baseURL: 'http://localhost:8080',
-        });
-    }
+class EmployeeService {
+  constructor() {
+    this.axios = axios.create({
+      baseURL: "http://localhost:8080",
+    });
+  }
+
+  getAllEmployees(){
+    return this.axios.get('/employees')
+      .then(({ data: employees }) => employees);
+  }
 }
+
+const employeeService = new EmployeeService();
+
+export default employeeService;
