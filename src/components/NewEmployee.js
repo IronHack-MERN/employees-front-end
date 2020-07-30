4ab0404cf3a22bed8a8c5b2bcccd691ebdfeb51f
Elena Piaggio <elenapiaggio@gmail.com>
2020-07-30 23:58:32 +0200

    UPDATE NewEmployee

--- src/components/NewEmployee.js
+++ src/components/NewEmployee.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import EmployeeService from "../services/employeeService";
-import Home from '../components/Home';
+import Home from "../components/Home";
 
 class NewEmployee extends Component {
   state = {
@@ -32,109 +32,109 @@ class NewEmployee extends Component {
   render() {
     return (
       <div>
-        <Home/>
+        <Home />
         <div className="form-wrapper">
-      
-        <h1 className="title">New Employee</h1>
+          <h1 className="title">New Employee</h1>
 
-        <div className="">
-          <form onSubmit={this.handleFormSubmit}>
-            <div className="field is-horizontal">
-              <div class="field-label">
-                <label className="label is-medium">Name</label>
-              </div>
+          <div className="">
+            <form onSubmit={this.handleFormSubmit}>
+              <div className="field is-horizontal">
+                <div className="field-label">
+                  <label className="label is-medium">Name</label>
+                </div>
 
-              <div class="field-body">
-                <div className="field">
-                  <div className="control">
-                    <input
-                      className="input is-medium"
-                      placeholder="John"
-                      type="text"
-                      name="name"
-                      value={this.state.name}
-                      onChange={(e) => this.handleChange(e)}
-                    />
+                <div className="field-body">
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input is-medium"
+                        placeholder="John"
+                        type="text"
+                        name="name"
+                        value={this.state.name}
+                        onChange={(e) => this.handleChange(e)}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="field is-horizontal">
-              <div class="field-label">
-                <label className="label is-medium">Surname</label>
-              </div>
-              <div class="field-body">
-                <div className="field">
-                  <div className="control">
-                    <input
-                      className="input is-medium"
-                      placeholder="Doe"
-                      type="text"
-                      name="surname"
-                      value={this.state.surname}
-                      onChange={(e) => this.handleChange(e)}
-                    />
+              <div className="field is-horizontal">
+                <div className="field-label">
+                  <label className="label is-medium">Surname</label>
+                </div>
+                <div className="field-body">
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input is-medium"
+                        placeholder="Doe"
+                        type="text"
+                        name="surname"
+                        value={this.state.surname}
+                        onChange={(e) => this.handleChange(e)}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="field is-horizontal">
-              <div class="field-label">
-                <label className="label is-medium">Job</label>
-              </div>
-              <div class="field-body">
-                <div className="field">
-                  <div className="control">
-                    <input
-                      className="input is-medium"
-                      placeholder="Developer, Software enginyeer"
-                      type="text"
-                      name="job"
-                      value={this.state.job}
-                      onChange={(e) => this.handleChange(e)}
-                    />
+              <div className="field is-horizontal">
+                <div className="field-label">
+                  <label className="label is-medium">Job</label>
+                </div>
+                <div className="field-body">
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input is-medium"
+                        placeholder="Developer, Software enginyeer"
+                        type="text"
+                        name="job"
+                        value={this.state.job}
+                        onChange={(e) => this.handleChange(e)}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="field is-horizontal">
-              <div class="field-label">
-                <label className="label is-medium">Date</label>
-              </div>
-              <div class="field-body">
-                <div className="field">
-                  <div className="control">
-                    <input
-                      className="input is-medium"
-                      placeholder="20-10-2020"
-                      type="text"
-                      name="createdAt"
-                      value={this.state.createdAt}
-                      onChange={(e) => this.handleChange(e)}
-                    />
+              <div className="field is-horizontal">
+                <div className="field-label">
+                  <label className="label is-medium">Date</label>
+                </div>
+                <div className="field-body">
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input is-medium"
+                        placeholder="20-10-2020"
+                        type="text"
+                        name="createdAt"
+                        value={this.state.createdAt}
+                        onChange={(e) => this.handleChange(e)}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div class="field is-horizontal">
-              <div class="field-label"></div>
-              <div class="field-body">
-                <div class="field">
-                  <div class="control">
-                    <button class="button is-primary btn-add-employee">ADD NEW EMPLOYEE</button>
+              <div className="field is-horizontal">
+                <div className="field-label"></div>
+                <div className="field-body">
+                  <div className="field">
+                    <div className="control">
+                      <button className="button is-primary btn-add-employee">
+                        ADD NEW EMPLOYEE
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-      </div>
-      
     );
   }
 }
