0bdfd890a7e36f5bcd01a005ba854b7286ca8ea7
Elena Piaggio <elenapiaggio@gmail.com>
2020-07-30 23:58:03 +0200

    UPDATE routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import AllEmployees from './components/AllEmployees';
-import EmployeeDetail from './components/EmployeeDetail';
+import AllEmployees from "./components/AllEmployees";
+import EmployeeDetail from "./components/EmployeeDetail";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
-import NewEmployee from './components/NewEmployee';
-import Header from './components/Header';
+import NewEmployee from "./components/NewEmployee";
+import UpdateEmployee from "./components/UpdateEmployee";
+import UpdateEmployeeDetail from "./components/UpdateEmployeeDetail";
+import Header from "./components/Header";
 
 import "./App.css";
 
@@ -13,12 +15,17 @@ class App extends Component {
     return (
       <div className="App">
         <BrowserRouter>
-        <Header/>
+          <Header />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path='/employees' component={AllEmployees} />
+            <Route exact path="/employees" component={AllEmployees} />
             <Route exact path="/employees/:id" component={EmployeeDetail} />
-            <Route exact path='/create' component={NewEmployee} />
+
+            <Route exact path="/create" component={NewEmployee} />
+            <Route exact path="/create/:id" component={NewEmployee} />
+
+            <Route exact path="/update" component={UpdateEmployee} />
+            <Route exact path="/udpate/:id" component={UpdateEmployeeDetail} />
           </Switch>
         </BrowserRouter>
       </div>
